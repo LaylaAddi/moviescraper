a1848652702fc094620d12ff471642888df8f383
steve chesnowitz <s.chesnowitz@gmail.com>
2016-02-09 03:38:10 +0000

    paperclup model update

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,5 +3,7 @@ class Movie < ActiveRecord::Base
   validates_presence_of :title
   validates :hotness, :numericality => {:allow_blank => true}
 	validates :image_url, :url => {:allow_blank => true}
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x150>" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 	
 end
--- gp
+++ gp
@@ -1,5 +1,5 @@
 git add -A
-git commit -m "add send grid, aws and dotenv-rails "
+git commit -m "paperclup model update "
 git push -u origin master
 git push heroku master
 
