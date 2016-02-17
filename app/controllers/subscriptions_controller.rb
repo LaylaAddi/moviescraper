eefca2007d677f86b9becc135c3eeafd84e85398
steve chesnowitz <s.chesnowitz@gmail.com>
2016-02-17 22:04:29 +0000

    customer able to update cc details

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -8,58 +8,42 @@ class SubscriptionsController < ApplicationController
   def new
   end
 
-  # def create
-  #   customer = if current_user.stripe_id?
-  #               Stripe::Customer.retrieve(current_user.stripe_id)
-  #             else
-  #               Stripe::Customer.create(email: current_user.email)
-  #             end
-
   def create
     customer = if current_user.stripe_id?
-                Stripe::Customer.retrieve(current_user.stripe_id)
-              else
-                Stripe::Customer.create(email: current_user.email)
-              end
-      
-    
-    subscription = Stripe::Customer.create(
-      email: current_user.email,
+                 Stripe::Customer.retrieve(current_user.stripe_id)
+               else
+                 Stripe::Customer.create(email: current_user.email)
+               end
+
+    subscription = customer.subscriptions.create(
       source: params[:stripeToken],
-      plan: "2" 
+      plan: "2"
     )
-    current_user.update(
+
+    options = {
       stripe_id: customer.id,
       stripe_subscription_id: subscription.id,
+    }
+
+    # Only update the card on file if we're adding a new one
+    options.merge!(
       card_last4: params[:card_last4],
       card_exp_month: params[:card_exp_month],
       card_exp_year: params[:card_exp_year],
-      card_brand: params[:card_brand]      
-    )
+      card_brand: params[:card_brand]
+    ) if params[:card_last4]
+
+    current_user.update(options)
+
     redirect_to root_path
   end
 
-    # options = {
-    #   stripe_id: customer.id,
-    #   stripe_subscription_id: subscription.id,
-    # }
-
-    # # Only update the card on file if we're adding a new one
-    # options.merge!(
-
-    # ) if params[:card_last4]
-
-    # current_user.update(options)
-
-    # redirect_to root_path
-  
-
   def destroy
-    # customer = Stripe::Customer.retrieve(current_user.stripe_id)
-    # customer.subscriptions.retrieve(current_user.stripe_subscription_id).delete
-    # current_user.update(stripe_subscription_id: nil)
+    customer = Stripe::Customer.retrieve(current_user.stripe_id)
+    customer.subscriptions.retrieve(current_user.stripe_subscription_id).delete
+    current_user.update(stripe_subscription_id: nil)
 
-    # redirect_to root_path, notice: "Your subscription has been canceled."
+    redirect_to root_path, notice: "Your subscription has been canceled."
   end
 
   private
--- gp
+++ gp
@@ -1,5 +1,5 @@
 git add -A
-git commit -m "moved figaro to development"
-git push origin master
+git commit -m "customer able to update cc details"
+git push origin stripe2
 git push heroku master
 
